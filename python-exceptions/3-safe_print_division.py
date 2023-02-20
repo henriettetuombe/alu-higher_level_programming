49e1c82649b6166684ccc99c2343da7b16e99687
henriettetuombe <h.tuombe@alustudent.com>
2023-02-20 05:39:54 -0800

    verify

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    div = 1
+    try:
+        div = a / b
+
+    except:
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+        return div
