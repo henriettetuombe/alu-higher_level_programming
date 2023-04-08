c5bb6f368ac1b941950ee503f77329525f76e973
henriettetuombe <h.tuombe@alustudent.com>
2023-04-07 23:54:07 -0700

    wurr

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-# 100-matrix_mul.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a matrix multiplication function."""
+# function that multiplies 2 matrices
+"""
+    Define 'matrix_mul' function.
+"""
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is empty.
-        TypeError: If either m_a or m_b has different-sized rows.
-        ValueError: If m_a and m_b cannot be multiplied.
-    Returns:
-        A new matrix representing the multiplication of m_a by m_b.
+    """
+        Multiply two matrices.
+        Args:
+            m_a (list of lists of ints/floats): The first matrix.
+            m_b (list of lists of ints/floats): The second matrix.
+        Return:
+            new matrix representing the multiplication of m_a by m_b.
     """
 
     if m_a == [] or m_a == [[]]:
@@ -42,9 +38,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
@@ -65,5 +61,4 @@ def matrix_mul(m_a, m_b):
                 prod += row[i] * col[i]
             new_row.append(prod)
         new_matrix.append(new_row)
-
     return new_matrix
